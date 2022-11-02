d19786f4ac10cc1900ad02e0835e297721faed08
Justin Lai <P2724060@LEC-ADMIN.dmu.ac.uk>
2022-11-02 10:50:41 +0000

    InputManager

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerInput playerInput;
+    private void Start()
+    {
+        playerInput = new();
+        
+    }
+}
